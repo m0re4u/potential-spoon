8ec986ed13961478159b13a0c2e69c2e7fadb86a
Michiel van der Meer <michielmeer@live.nl>
2017-05-03 17:09:51 +0200

    Larger network and post/pre synaptic modelling

--- synapses/LIFNeuron.cpp
+++ synapses/LIFNeuron.cpp
@@ -7,53 +7,90 @@
 #include <vector>
 #include <math.h>
 #include <stdlib.h>
+#include <tuple>
 
 #include "LIFNeuron.h"
+
+#define N_NEURONS 6
 
 double deltaV(double I, double V, int tau) {
   return (I - V) / tau;
 }
 
-int main(int argc, char const *argv[]) {
-  // setup parameters and state variables
-  int N = 3; // number of neurons
-  int T = 100; // total time to simulate
-  int ms = 1; // simulation time step
+double clip(double low, double high, double value) {
+  if (value > high) {
+    return high;
+  } else if (value < low) {
+    return low;
+  } else {
+    return value;
+  }
+}
 
-  neuron neurons[3];
+int main(int argc, char const *argv[]) {
+  int print_neuron = 0;
+  if (argc == 2) {
+    print_neuron = atoi(argv[1]);
+  }
+  // setup parameters and state variables
+  int N = N_NEURONS; // number of neurons
+  int T = 1000; // total time to simulate
+  int ms = 1; // simulation time step
+  double taupre = 20*ms;
+  double taupost = 20*ms;
+  double A_pre = 0.01; // update set for STDP for presynaptics
+  double A_post = -A_pre * taupre / taupost * 1.05; // update set for STDP for prostsynaptics
+  double w_max = 2; // maximum weight
+
+  neuron neurons[N_NEURONS];
+
   // Set neuron specific parameters
   neurons[0].tau = 10;
-  neurons[1].tau = 100;
-  neurons[2].tau = 100;
   neurons[0].I = 2;
-  neurons[1].I = 0;
-  neurons[2].I = 0;
 
   // Matrix indicating the connection weights between neurons
-  double connectionMatrix[3][3] = {{0,0.2,0.4},{0,0,0},{0,0,0}};
+  double connectionMatrix[N_NEURONS][N_NEURONS] =
+    { {0,1,0,0,0,0},
+      {0,0,1,0,0,0},
+      {0,0,0,1,0,0},
+      {0,0,0,0,1,0},
+      {0,0,0,0,0,1},
+      {0,0,0,0,0,0} };
 
   // Simulate time ticks
   for (size_t t = 0; t < T; t += ms) {
     // Simulate each neuron
     for (size_t n = 0; n < N; n++) {
-      // Assign possible incoming spikes
-      neurons[n].V += neurons[n].incomingSpike;
-      neurons[n].incomingSpike = 0;
+      // Handle presynaptics spikes
+      if (neurons[n].incoming_spikes.size() >= 1) {
+        for (auto& pre_spiked : neurons[n].incoming_spikes) {
+          neurons[n].a_pre += A_pre;
+          double new_w = connectionMatrix[std::get<0>(pre_spiked)][n] += neurons[n].a_post;
+          connectionMatrix[std::get<0>(pre_spiked)][n] = clip(0, w_max, new_w);
+          neurons[n].V += std::get<1>(pre_spiked);
+        }
+      }
+      neurons[n].incoming_spikes.clear();
 
-      if (n == 2) {
+      if (argc > 1 && n == print_neuron) {
         std::cout << t << ", " << neurons[n].V << '\n';
       }
 
       // Check if the neuron fires
       if (neurons[n].V > neurons[n].threshold) {
         // std::cout << "Neuron " << n << " spiked at " << t << " with value " << neurons[n].V << '\n';
-        neurons[n].spikes.push_back(t);
+        // Set refractory period
         neurons[n].refractory = 3*ms;
-
+        // Postsynaptic spike occurs
+        neurons[n].a_post += A_post;
         // Spike to connected neurons
         for (size_t p = 0; p < N; p++) {
           if (connectionMatrix[n][p] != 0) {
-            neurons[p].incomingSpike += connectionMatrix[n][p];
+            // std::cout << "Spiking to: " << p << " with weight "
+            //           << connectionMatrix[n][p] << '\n';
+            neurons[p].incoming_spikes.push_back(std::make_tuple(n, connectionMatrix[n][p]));
+            // Update weight for synapse
+            connectionMatrix[n][p] += neurons[n].a_pre;
           }
         }
         neurons[n].V = 0;
--- synapses/LIFNeuron.h
+++ synapses/LIFNeuron.h
@@ -3,14 +3,17 @@
  * Author: Michiel van der Meer
  */
 struct neuron {
-   neuron() : threshold(1), refractory(0), V(0), tau(10), incomingSpike(0) {}
-   double threshold; // threshold
-   double refractory; // refractory period counter
-   double V; // current potential
-   double I; // current impulse
-   int tau; // timing constant
-   std::vector<unsigned> spikes;
-   double incomingSpike; // incoming spike from the last cycle
+  neuron() : threshold(1), refractory(0), V(0), I(0), tau(100), a_pre(0), a_post(0) {}
+  double threshold; // threshold
+  double refractory; // refractory period counter
+  double V; // current potential
+  double I; // current impulse
+  int tau; // timing constant
+  // std::vector<unsigned> spikes;
+  std::vector<std::tuple<int, double>> incoming_spikes; // incoming spikes from the last cycle
+
+  double a_pre; // trace of pre synaptic activity
+  double a_post; // trace of post synaptic activity
 };
 
 /**
